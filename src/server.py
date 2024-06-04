fa1af0118c6b25d9b2d43f4f6b321e8e4325d0b5
Sebástian Narváez <sebnarvaez19@gmail.com>
2024-06-04 16:06:36 -0500

    feat: read letter content

--- src/server.py
+++ src/server.py
@@ -2,6 +2,8 @@
 
 from shiny import Inputs, render
 
+from utils import read_letter
+
 
 def server(inputs: Inputs):
     """Main Server function for Shiny App.
@@ -14,13 +16,6 @@ def server(inputs: Inputs):
 
     @render.text
     def letter() -> str:
-        return """
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            It has survived not only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged.
-            
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        """
+        letter = inputs.file_select()
+        content = read_letter(letter)
+        return content
